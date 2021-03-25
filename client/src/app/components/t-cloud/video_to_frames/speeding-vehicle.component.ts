6e1edc4ddf0fd3636b4c18cea3a5b52624c23a37
Dhananjay <dhananjay@graymatics.com>
2021-03-25 18:22:59 +0530

    Changes

--- client/src/app/components/t-cloud/video_to_frames/speeding-vehicle.component.ts
+++ client/src/app/components/t-cloud/video_to_frames/speeding-vehicle.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AnnotationsService } from '../../../services/annotations.service';
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
+import { FacesService } from 'src/app/services/faces.service';
+import JSMpeg from '@cycjimmy/jsmpeg-player';
 
 @Component({
   selector: 'app-speeding-vehicle',
@@ -9,70 +12,133 @@ import { AnnotationsService } from '../../../services/annotations.service';
 })
 export class SpeedingVehicleComponent implements OnInit {
 
-conf: any = {
-  ss: 0,
-  t: 0,
-  fps: 1,
-  name: ''
-};
-ss:number;
-t:number;
-title:string;
-start:any;
-finish:any;
-act:boolean = false;
+  conf: any = {
+    ss: 0,
+    t: 0,
+    fps: 1,
+    name: '',
+    stream: ''
+  };
+  ss: number;
+  t: number;
+  title: string;
+  start: any;
+  finish: any;
+  act: boolean = false;
+  cameras: any;
+  cam_name: any;
+  rtsp_in: any;
+  player: any;
+  link: SafeResourceUrl;
 
+  @ViewChild('streaming', {static: false}) streamingcanvas: ElementRef;
+  
   constructor(
-    private activatedRoute: ActivatedRoute, 
-    private annotationservice: AnnotationsService, 
-    private router: Router
-    ) { }
-
-  ngOnInit(){
-    this.title = this.activatedRoute.snapshot.params.cam_name + ' at ' + this.activatedRoute.snapshot.params.date;
+    private activatedRoute: ActivatedRoute,
+    private annotationservice: AnnotationsService,
+    private router: Router,
+    sanitizer: DomSanitizer,
+    private facesservices: FacesService
+  ) {
+    this.link = sanitizer.bypassSecurityTrustStyle('url(' + 'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov' + ')');
   }
 
-  change(i,t){
-    if(i.length == 5){
-    i = i + ':00'
+  ngOnInit() {
+    this.cam_name = this.activatedRoute.snapshot.params.cam_name;
+    this.getCameraList();
+    this.title = this.cam_name + ' at ' + this.activatedRoute.snapshot.params.date;
+  }
+
+  ngOnDestroy(){
+    this.destroy();
+  }
+
+  destroy(){
+    if(this.player != undefined){
+      this.player.destroy();
+      this.player = null;
+      let cam = this.cameras.filter(element => element.name === this.cam_name);
+      this.rtsp_in = cam[0].rtsp_in;
+      let data = {
+        camera_name: cam[0].name,
+        rtsp_in: cam[0].rtsp_in,
+        id: cam[0].id
+      };
+      this.facesservices.stopWsStream(data).subscribe(
+        res =>{
+        },
+        err=> console.error(err)
+      )
+    }
+  }
+
+  getCameraList() {
+    this.facesservices.getCameras().subscribe(
+      res => {
+        this.cameras = res;
+        this.loadLiveCam();
+      },
+      err => console.log(err)
+    )
+  }
+
+  loadLiveCam() {
+    let cam = this.cameras.filter(element => element.name === this.cam_name);
+    this.rtsp_in = cam[0].rtsp_in;
+    let data = {
+      camera_name: cam[0].name,
+      rtsp_in: cam[0].rtsp_in,
+      id: cam[0].id
+    };
+    this.facesservices.startWsStream(data).subscribe(
+      res =>{
+        this.player = new JSMpeg.Player(`ws://${res['my_ip']}:${res['port']}`, {
+          canvas: this.streamingcanvas.nativeElement, autoplay: true, audio: false, loop: true
+        })
+      },
+      err=> console.error(err)
+    )
+  }
+
+  change(i, t) {
+    if (i.length == 5) {
+      i = i + ':00'
     }
     const time = i.split(':');
     time[0] = +time[0];
     time[1] = +time[1];
     time[2] = +time[2];
-    if(t == 's'){
-      if(this.start.length == 5){
+    if (t == 's') {
+      if (this.start.length == 5) {
         this.start = this.start + ':00'
       }
-        this.ss = time[0]*60*60 + time[1]*60 +time[2];
-      }else if( t == 'f'){
-        if(this.finish.length == 5){
-          this.finish = this.finish + ':00'
-        }
-      this.t = time[0]*60*60 + time[1]*60 +time[2];
+      this.ss = time[0] * 60 * 60 + time[1] * 60 + time[2];
+    } else if (t == 'f') {
+      if (this.finish.length == 5) {
+        this.finish = this.finish + ':00'
+      }
+      this.t = time[0] * 60 * 60 + time[1] * 60 + time[2];
     }
-    if(this.start != undefined && this.finish != undefined){
+    if (this.start != undefined && this.finish != undefined) {
       this.act = true;
     }
   }
 
-  send(){
-    this.conf.name = this.title;
+  send() {
+    let cam = this.cameras.filter(element => element.name === this.cam_name);
     this.conf.ss = this.ss;
+    this.conf.cam_id = cam[0].id;
     this.conf.t = this.t - this.ss;
-
-    //test no real code
-    this.conf.name = 'video.mov'
-    //end of test
-
-    this.annotationservice.cutVideo(this.conf).subscribe(
-      res=>{
-        console.log(res)
+    this.conf.name = this.cam_name;
+    this.conf.stream = this.rtsp_in;
+    console.log('>>>>>>>>>>>', this.conf);
+    this.router.navigate(['/annotations'])
+    /* this.annotationservice.cutVideo(this.conf).subscribe(
+      res => {
         this.router.navigate(['/annotations'])
       },
       err => console.log(err)
-    )
-    console.log(this.conf)
+    ) */
   }
 
 }
--- client/src/app/services/annotations.service.ts
+++ client/src/app/services/annotations.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ip } from '../models/IpServer';
+import { vistaIP } from '../models/VistaServer';
 
 
 @Injectable({
@@ -9,13 +10,18 @@ import { ip } from '../models/IpServer';
 export class AnnotationsService {
 
   API_URL = 'http://'+ ip +':3300/api';
+  VISTA_API_URL = vistaIP;
 
   getImages(where:string,info:string){
     return this.http.get(`${this.API_URL}/getImages/${where}/${info}`);
   }
 
   getDatasets(which:string){
-    return this.http.get(`${this.API_URL}/getFolders/${which}`);
+    return this.http.get(`${this.API_URL}/datasets/${which}`);
+  }
+
+  getOperation(id: number) {
+    return this.http.get(`${this.API_URL}/api/v1/operation/${id}`);
   }
 
   readLabels(){
@@ -35,12 +41,23 @@ export class AnnotationsService {
   }
 
   cutVideo(conf: any){
-    return this.http.post(`${this.API_URL}/cortarFrames/`, conf);
+    return this.http.post(`${this.API_URL}/datasets/cortarFrames/`, conf);
   }
 
   moveImage(path:string, name:string, dec:boolean){
     return this.http.get(`${this.API_URL}/classify/${path}/${name}/${dec}`);
   }
 
+  createDataset(data:any){
+    return this.http.post(`${this.API_URL}/dataset/create/`, data);
+  }
+
+  processDataset(data:any) {
+    return this.http.post(`${this.API_URL}/datasets/process/`, data);
+  }
+
+  searchImages(keyword:any) {
+    return this.http.get(`${this.API_URL}/search/${keyword}`);
+  }
   constructor(private http: HttpClient) { }
 }
