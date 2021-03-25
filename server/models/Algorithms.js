f57bd7264b7048e9453a4e2b910e21c833b82644
Dhananjay <dhananjay@graymatics.com>
2021-03-25 18:28:43 +0530

    Changes

--- server/models/Algorithms.js
+++ server/models/Algorithms.js
@@ -1,10 +1,14 @@
-var db=require('../dbconnection'); //reference of dbconnection.js
+var db = require('../dbconnection'); //reference of dbconnection.js
 
-var Algorithm={
+var Algorithm = {
 
-list:function(callback){
-return db.query('SELECT * FROM algorithms ORDER BY id', callback);
-}
+    list: function (callback) {
+        return db.query('SELECT * FROM algorithms ORDER BY id', callback);
+    },
+
+    fetchAlgoData: function (data, callback) {
+        return db.query(`SELECT * FROM ${data.table} where cam_id='${data.id}' ORDER BY time asc`, callback);
+    }
 };
 
- module.exports=Algorithm;
+module.exports = Algorithm;
