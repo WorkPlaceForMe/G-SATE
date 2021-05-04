e2d92234a5cd753d874d0e59dc28521f3a34310d
Abhratanu Mukherjee <abhratanu@graymatics.com>
2021-05-04 21:56:35 +0530

    Phase 1 issue resolve

--- client/src/app/services/fr.service.ts
+++ client/src/app/services/fr.service.ts
@@ -7,38 +7,38 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FrService {
 
-  ip:string;
-  API_URL:string = 'http://'+ this.ip +':8080/api/v1';
+  ip: string;
+  API_URL: string = 'http://' + this.ip + ':8080/api/v1';
 
 
-  faceRetrieval(req:any,ip){
+  faceRetrieval(req: any, ip) {
     return this.http.post(`${this.API_URL}/retrieval/base64_image`, req);
   }
 
-  delFaceGroup(group:any,ip){
+  delFaceGroup(group: any, ip) {
     return this.http.post(`${this.API_URL}/group/delete`, group);
   }
 
-  listFace(query:any,ip){
+  listFace(query: any, ip) {
     return this.http.post(`${this.API_URL}/group/query`, query);
   }
-  
-  updateFace(face:any,ip){
+
+  updateFace(face: any, ip) {
     return this.http.post(`${this.API_URL}/face/update`, face);
   }
-  
-  queryFace(del:any,ip){
+
+  queryFace(del: any, ip) {
     return this.http.post(`${this.API_URL}/face/query`, del);
   }
-  
-  deleteFace(del:any,ip){
+
+  deleteFace(del: any, ip) {
     return this.http.post(`${this.API_URL}/face/delete`, del);
   }
 
-  faceInsert(face:any,ip){
+  faceInsert(face: any, ip) {
     return this.http.post(`${this.API_URL}/face/insert`, face);
   }
-    
+
 
 
   constructor(private http: HttpClient) { }
