b6d4e20521e7bdea67158995dd6ea0aa495ff6a6
Abhratanu Mukherjee <abhratanu@graymatics.com>
2021-05-06 21:12:46 +0530

    Added Pre loading strategy

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router'; 
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router'; 
 import { FaceFormComponent } from './components/facial_recognition/face-form/face-form.component';
 import { ImagesFormComponent } from './components/facial_recognition/images-form/images-form.component';
 import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
@@ -159,7 +159,14 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(
+      routes,
+      {
+        preloadingStrategy: PreloadAllModules
+      }
+    )
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
