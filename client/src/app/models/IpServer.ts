880147cc8ef7e5a35a727247943bd74d842caa17
abhratanu-graymatics <abhratanu@graymatics.com>
2021-05-10 12:39:17 +0530

    Azure URL Updated

--- client/src/app/models/IpServer.ts
+++ client/src/app/models/IpServer.ts
@@ -1,3 +1,4 @@
-export var ip :string ='3.211.144.140';
+export var ip :string ='40.112.52.108';
+// export var ip :string ='3.211.144.140';
 // export var ip :string ='localhost';
 // ip of where the server is running 13.52.209.143
