8a637e5148a5e49072be0e8747ae8846cea37485
Abhratanu Mukherjee <abhratanu@graymatics.com>
2021-06-10 12:37:19 +0530

    Update Azure IPs

--- client/src/app/models/IpServer.ts
+++ client/src/app/models/IpServer.ts
@@ -1,4 +1,5 @@
 // export var ip :string ='40.112.52.108'; // Azure
-export var ip: string = "3.211.144.140"; // AWS
-// export var ip :string ='localhost';
+export var ip :string ='20.41.8.149'; // Azure running
+//export var ip: string = "3.211.144.140"; // AWS
+// export var ip: string = "localhost";
 // ip of where the server is running 13.52.209.143
