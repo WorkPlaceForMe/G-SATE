301dc8c969415c5b0e90aaef416c685cd960819e
Abhratanu Mukherjee <abhratanu@graymatics.com>
2021-06-14 12:18:58 +0530

    Update annotations

--- server/routes/Annotation.js
+++ server/routes/Annotation.js
@@ -17,22 +17,6 @@ router.get("/models", function (req, res, next) {
 
 router.post("/confirmed", function (req, res, next) {
   let body = req.body;
-
-  /**
-   * Update ratio of (x, y) coordinates as per actual image width height
-   */
-  for (const index in body.data) { 
-    const SET = body.data[index];
-    for (let i = 0; i < SET.results.length; i++) {
-      for (let j = 0; j < SET.results[i].length; j++) {
-        if (SET.results[i][j].x && SET.results[i][j].y) {
-          body.data[index].results[i][j].x = (SET.results[i][j].x / SET.canvas_width) * SET.width;
-          body.data[index].results[i][j].y = (SET.results[i][j].y / SET.canvas_height) * SET.height;
-        }
-      }
-    }
-  }
-
   let directory =
     process.env.resources2 + "training_details/" + body.datasetName + ".json";
   fs.writeFileSync(directory, JSON.stringify(body.data));
@@ -51,6 +35,23 @@ router.post("/confirmed", function (req, res, next) {
 router.post("/object-detection/confirmed", function (req, res, next) {
   console.log("object detection training confirmed");
   let body = req.body;
+
+  /**
+   * Update ratio of (x, y) coordinates as per actual image width height
+   */
+  for (const index in body.data) { 
+    const SET = body.data[index];
+    for (let i = 0; i < SET.results.length; i++) {
+      for (let j = 0; j < SET.results[i].length; j++) {
+        if (SET.results[i][j].x && SET.results[i][j].y) {
+          body.data[index].results[i][j].x = (SET.results[i][j].x / SET.canvas_width) * SET.width;
+          body.data[index].results[i][j].y = (SET.results[i][j].y / SET.canvas_height) * SET.height;
+        }
+      }
+    }
+  }
+  /** ---------------- */
+
   let directory =
     process.env.resources2 + "training_details/" + body.datasetName + ".json";
   fs.writeFileSync(directory, JSON.stringify(body.data));
@@ -60,7 +61,11 @@ router.post("/object-detection/confirmed", function (req, res, next) {
     if (err) {
       res.json(err);
     } else {
-      res.json(annotation);
+      // res.json(annotation); // OLD RESPONSE
+      /**
+       * Return body with updated (x, y) coordinates
+       */
+      res.json(body);
       //res.redirect("/camerasList");
     }
   });
