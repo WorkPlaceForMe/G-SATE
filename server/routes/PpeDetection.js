ad80a324a90e7ae5d72699e3c47e79de5d21f945
Abhratanu Mukherjee <abhratanu@graymatics.com>
2021-08-16 17:51:09 +0530

    Prepared PPE detection response

--- server/routes/PpeDetection.js
+++ server/routes/PpeDetection.js
@@ -9,13 +9,101 @@ router.post('/object/detection', function (req, res, next) {
   }
 
   const image = req.body.image;
+  let dir = "./objdet/darknet/data/" + image.split("/")[7];
   const command = `cd ./objdet/darknet && ./darknet detector test ../../../../kartik/graymatics.data ../../../../kartik/yolov3_deploy.cfg ../../../../kartik/yolov3_deploy.weights ${image} -ext_output`;
 
-  cp.exec(command, (err, data) => {
-  	if(err) {
-  	  res.status(400).send(err);
-  	}
+  saveImg(image, dir, function (err, data) {
+    cp.exec(command, (err, data) => {
+      console.log("Err LOG BY INT: ", err);
+      console.log("Data LOG BY INT: ", data);
+      
+      if(err) {
+        res.status(400).send(err);
+      }
 
-  	console.log(data);
-  })
+      console.log("type of data>>>>>>>>", typeof data);
+      console.log("data>>>>>>>>>>>>>>", data);
+      let result = [];
+      let single = [];
+
+      console.log("data splitted >>>>>>>>>>>", data.split("\n"));
+      data.split("\n").forEach((ele) => {
+        if (ele.includes("Predicted in") || ele == "") {
+          console.log("element >>>>>>>>>>>>", ele);
+        } else {
+          let itm = ele.trim().split(" ");
+          console.log("single image ? ", singleImage);
+          if (singleImage) {
+            console.log("in if");
+            let obj1 = {
+              x: itm[0],
+              y: itm[1],
+            };
+            single.push(obj1);
+            let obj2 = {
+              x: itm[2],
+              y: itm[3],
+            };
+            single.push(obj2);
+            let obj3 = {
+              general_detection: "Yes",
+            };
+            single.push(obj3);
+            let obj4 = {
+              label: "",
+            };
+            single.push(obj4);
+            result.push(single);
+            single = [];
+          } else {
+            console.log("in else");
+            let obj1 = {
+              x: (itm[0] * details.width) / details.res_width,
+              y: (itm[1] * details.height) / details.res_height,
+            };
+            single.push(obj1);
+            let obj2 = {
+              x: (itm[2] * details.width) / details.res_width,
+              y: (itm[3] * details.height) / details.res_height,
+            };
+            single.push(obj2);
+            let obj3 = {
+              general_detection: "Yes",
+            };
+            single.push(obj3);
+            let obj4 = {
+              label: "",
+            };
+            single.push(obj4);
+            result.push(single);
+            single = [];
+          }
+        }
+      });
+      if (fs.existsSync(dir)) {
+        fs.unlinkSync(dir);
+      }
+      res.json(result);
+    })
+  });
 });
+
+let saveImg = function (uri, filePath, callback) {
+  try {
+    let options = {
+      url: uri,
+      strictSSL: false,
+    };
+    request.head(options, function (err, res, body) {
+      console.log('SAVE_IMAGE', err, res, body);
+
+      request(options)
+        .pipe(fs.createWriteStream(filePath))
+        .on("close", function () {
+          callback();
+        });
+    });
+  } catch (err) {
+    console.log("Error>>>>>>>>>", err);
+  }
+};
