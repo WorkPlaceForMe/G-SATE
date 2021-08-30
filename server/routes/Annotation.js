98615bbdae685573669e6ca7c3d715cf1b1ff37a
abhratanu-grymatics <abhratanu@graymatics.com>
2021-08-30 17:09:41 +0530

    elastic implemented

--- server/routes/Annotation.js
+++ server/routes/Annotation.js
@@ -2,7 +2,16 @@ const fs = require("fs");
 const express = require("express");
 const router = express.Router();
 const Annotation = require("../models/Annotation");
+const elastic = require("elasticsearch");
+const elasticIndex = "image_annotations";
+const elasticType = "annotaions";
+require("dotenv").config({
+  path: "./config.env",
+});
 //Unsure
+const client = elastic.Client({
+  node: `${process.env.host}/${process.env.server}`
+});
 
 router.get("/models", function (req, res, next) {
   Annotation.getModelDetails(function (err, annotation) {
@@ -17,23 +26,46 @@ router.get("/models", function (req, res, next) {
 
 router.post("/confirmed", function (req, res, next) {
   let body = req.body;
+  const elasticData = [];
 
   /**
    * Update ratio of (x, y) coordinates as per actual image width height
    */
-  for (const index in body.data) { 
+  for (const index in body.data) {
     const SET = body.data[index];
     for (let i = 0; i < SET.results.length; i++) {
       for (let j = 0; j < SET.results[i].length; j++) {
         if (SET.results[i][j].x && SET.results[i][j].y) {
-          body.data[index].results[i][j].x = (SET.results[i][j].x / SET.canvas_width) * SET.width;
-          body.data[index].results[i][j].y = (SET.results[i][j].y / SET.canvas_height) * SET.height;
+          body.data[index].results[i][j].x =
+            (SET.results[i][j].x / SET.canvas_width) * SET.width;
+          body.data[index].results[i][j].y =
+            (SET.results[i][j].y / SET.canvas_height) * SET.height;
         }
       }
+      elasticData.push(
+        { index: { _index: elasticIndex, _type: elasticType } },
+        {
+          image: SET.image,
+          width: SET.width,
+          height: SET.height,
+          canvas_width: SET.canvas_width,
+          canvas_height: SET.canvas_height,
+          data: [...SET.results[i]],
+        }
+      );
     }
   }
-  /** ---------------- */
+  /** ---------------- */ 
   
+  try {
+    await client.bulk({
+      body: elasticData,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send(err);
+  }
+
   let directory =
     process.env.resources2 + "training_details/" + body.datasetName + ".json";
   fs.writeFileSync(directory, JSON.stringify(body.data));
@@ -50,6 +82,27 @@ router.post("/confirmed", function (req, res, next) {
   });
 });
 
+router.get("image/:key", function (req, res, next) {
+  const { key } = req.params;
+  if (!key) return res.status(400).send('Search key is required');
+
+  try {
+    const result = await client.search({
+      index: elasticIndex,
+      pretty: true,
+      filter_path: "hits.hits._source*",
+      q: `data.label:${key}`,
+    });
+    const result = await client.bulk({
+      body: data,
+    });
+    res.status(200).send(result.hits.hits);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
+
 router.post("/object-detection/confirmed", function (req, res, next) {
   console.log("object detection training confirmed");
   let body = req.body;
@@ -57,13 +110,15 @@ router.post("/object-detection/confirmed", function (req, res, next) {
   /**
    * Update ratio of (x, y) coordinates as per actual image width height
    */
-  for (const index in body.data) { 
+  for (const index in body.data) {
     const SET = body.data[index];
     for (let i = 0; i < SET.results.length; i++) {
       for (let j = 0; j < SET.results[i].length; j++) {
         if (SET.results[i][j].x && SET.results[i][j].y) {
-          body.data[index].results[i][j].x = (SET.results[i][j].x / SET.canvas_width) * SET.width;
-          body.data[index].results[i][j].y = (SET.results[i][j].y / SET.canvas_height) * SET.height;
+          body.data[index].results[i][j].x =
+            (SET.results[i][j].x / SET.canvas_width) * SET.width;
+          body.data[index].results[i][j].y =
+            (SET.results[i][j].y / SET.canvas_height) * SET.height;
         }
       }
     }
@@ -93,12 +148,12 @@ router.post("/time-slot/save", function (req, res) {
   res.json({
     inferencing_time_slot: {
       start_time: req.body.start_time,
-      end_time: req.body.end_time
+      end_time: req.body.end_time,
     },
     traning_time_slot: {
       start_time: req.body.start_time,
-      end_time: req.body.end_time
-    }
+      end_time: req.body.end_time,
+    },
   });
 });
 
