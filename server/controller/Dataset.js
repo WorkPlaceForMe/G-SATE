8b8e0fd3ec57c5829bbefdcca0d822d235ddd172
Abhratanu Mukherjee <abhratanu@graymatics.com>
2021-09-21 19:07:22 +0530

    multiple vista con

--- server/controller/Dataset.js
+++ server/controller/Dataset.js
@@ -28,6 +28,42 @@ const makeRandomString = (length) => {
   return result.join("");
 };
 
+const operationFunction = async (data) => {
+  const responseArray =[]
+
+  const operationOptions = {
+    method: "GET",
+    url: '',
+    strictSSL: false,
+    headers: {
+      // Authorization: process.env.authorization,
+      "Content-Type": `application/json`,
+    },
+    auth: {
+      username: "admin",
+      password: "admin",
+    }
+  };
+
+  await data.forEach(async(element)=>{
+    console.log(element,'>>>>>>>>>>>>>>>>>forEach');
+    console.log(element.id,'>>>>>>>>>>>>>>>>>>>>>>>>elem>>>idd');
+    operationOptions['url'] = process.env.vista_server_ip + '/api/v1/operation/' + element.id;
+    console.log(operationOptions,'>>>>>>>>>>>>...operationOptions');
+    await rp(operationOptions)
+    .then((res) => {
+      console.log(res,'>>>>>>>>>295');
+      responseArray.push(res)
+      console.log(responseArray,'>>>>>>>>297');
+    })
+    .catch((error) => {
+      return res.status(500).json(error);
+    });
+    })
+    console.log(responseArray,'>>>>>>>>responseArray');
+    return responseArray
+}
+
 let Dataset = {
   processWithoutVista: async (req, res, next) => {
     let body = req.body;
@@ -224,83 +260,53 @@ let Dataset = {
       });
 
       console.log(responseData,'>>>>>>>>>>>>responseData');
-      const responseArray = []
-      const operationOptions = {
-        method: "GET",
-        url: '',
-        strictSSL: false,
-        headers: {
-          // Authorization: process.env.authorization,
-          "Content-Type": `application/json`,
-        },
-        auth: {
-          username: "admin",
-          password: "admin",
-        }
-      };
+      // const responseArray = []
+      // const operationOptions = {
+      //   method: "GET",
+      //   url: '',
+      //   strictSSL: false,
+      //   headers: {
+      //     // Authorization: process.env.authorization,
+      //     "Content-Type": `application/json`,
+      //   },
+      //   auth: {
+      //     username: "admin",
+      //     password: "admin",
+      //   }
+      // };
 
       const data = JSON.parse(responseData)
       
       if(data && data.length > 0){
         console.log(data.length,'>>>>>>>responseData.length');
         console.log(typeof data); 
-        data.forEach(async(element)=>{
-        console.log(element,'>>>>>>>>>>>>>>>>>forEach');
-        console.log(element.id,'>>>>>>>>>>>>>>>>>>>>>>>>elem>>>idd');
-        operationOptions['url'] = process.env.vista_server_ip + '/api/v1/operation/' + element.id;
-        console.log(operationOptions,'>>>>>>>>>>>>...operationOptions');
-        await rp(operationOptions)
-        .then((res) => {
-          console.log(res,'>>>>>>>>>260');
-          responseArray.push(res)
-          console.log(responseArray,'>>>>>>>>261');
-        })
-        .catch((error) => {
-          return res.status(500).json(error);
-        });
-        })
+        // data.forEach(async(element)=>{
+        // console.log(element,'>>>>>>>>>>>>>>>>>forEach');
+        // console.log(element.id,'>>>>>>>>>>>>>>>>>>>>>>>>elem>>>idd');
+        // operationOptions['url'] = process.env.vista_server_ip + '/api/v1/operation/' + element.id;
+        // console.log(operationOptions,'>>>>>>>>>>>>...operationOptions');
+        // await rp(operationOptions)
+        // .then((res) => {
+        //   console.log(res,'>>>>>>>>>260');
+        //   responseArray.push(res)
+        //   console.log(responseArray,'>>>>>>>>261');
+        // })
+        // .catch((error) => {
+        //   return res.status(500).json(error);
+        // });
+        // })
 
-        console.log(responseArray,'>>>>>>>>>>>269');
-        return res.json(responseArray);
+        await operationFunction(data).then((value) => {
+          console.log(value,'>>>>>>>>>>>301');
+          return res.json(responseArray);
+        }
+        )
+
+        // console.log(responseArray,'>>>>>>>>>>>269');
+        // return res.json(responseArray);
       }
 
   },
-
-  // operationFunction :async (data) => {
-  //   const responseArray =[]
-
-  //   const operationOptions = {
-  //     method: "GET",
-  //     url: '',
-  //     strictSSL: false,
-  //     headers: {
-  //       // Authorization: process.env.authorization,
-  //       "Content-Type": `application/json`,
-  //     },
-  //     auth: {
-  //       username: "admin",
-  //       password: "admin",
-  //     }
-  //   };
-
-  //   await data.forEach(async(element)=>{
-  //     console.log(element,'>>>>>>>>>>>>>>>>>forEach');
-  //     console.log(element.id,'>>>>>>>>>>>>>>>>>>>>>>>>elem>>>idd');
-  //     operationOptions['url'] = process.env.vista_server_ip + '/api/v1/operation/' + element.id;
-  //     console.log(operationOptions,'>>>>>>>>>>>>...operationOptions');
-  //     await rp(operationOptions)
-  //     .then((res) => {
-  //       console.log(res,'>>>>>>>>>295');
-  //       responseArray.push(res)
-  //       console.log(responseArray,'>>>>>>>>297');
-  //     })
-  //     .catch((error) => {
-  //       return res.status(500).json(error);
-  //     });
-  //     })
-  //     console.log(responseArray,'>>>>>>>>responseArray');
-  //     return responseArray
-  // },
 
   processVistaBatchImages: async (req, res, next) => {
     if (!req.body.images) {
