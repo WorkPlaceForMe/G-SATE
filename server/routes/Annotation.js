40d0a0e1ae0e826654b36fec4e1fe204af11309b
Abhratanu Mukherjee <abhratanu@graymatics.com>
2021-09-28 17:01:53 +0530

    modify search API

--- server/routes/Annotation.js
+++ server/routes/Annotation.js
@@ -67,33 +67,6 @@ const client = elastic.Client({
 //   }
 // }
 
-// const elasticSearchFunction = (index, type) => {
-//  return client
-//     .search({
-//       index: index,
-//       type: type,
-//       pretty: true,
-//       filter_path: 'hits.hits._source*',
-//       q: `class:${key}`,
-//       size: 10000,
-//     })
-//     .then(
-//       function (body) {
-//         if (body && body.hits && body.hits.hits && body.hits.hits.length > 0) {
-//           return body.hits.hits
-//         } else {
-//           return []
-//         }
-//       },
-//       function (error) {
-//         if (error) {
-//           console.log(error)
-//           res.status(500).send(error)
-//         }
-//       },
-//     )
-// }
-
 router.get('/models', function (req, res, next) {
   Annotation.getModelDetails(function (err, annotation) {
     console.log('get Model')
@@ -338,268 +311,6 @@ router.get('/vehicle/:key', function (req, res, next) {
 })
 
 router.get('/analytics/elasticSearch/:key', async function (req, res, next) {
-  const elasticVehicleIndex = 'vehicle_gsate'
-  const elasticPersonIndex = 'person_gsate'
-  const elasticClothingIndex = 'clothing_gsate'
-  const elasticPpeIndex = 'ppe_gsate'
-  const elasticDefectsIndex = 'defects_gsate'
-  const elasticSearchType = '_doc'
-
-  const { key } = req.params
-  if (!key) return res.status(400).send('Search key is required')
-
-  const resultArray = []
-
-  await Promise.all([
-    client
-      .search({
-        index: elasticVehicleIndex,
-        type: elasticSearchType,
-        pretty: true,
-        filter_path: 'hits.hits._source*',
-        q: `class:${key}`,
-        size: 10000,
-      })
-      .then(
-        function (body) {
-          if (
-            body &&
-            body.hits &&
-            body.hits.hits &&
-            body.hits.hits.length > 0
-          ) {
-            console.log('vehicle search function called')
-            resultArray.push(...body.hits.hits)
-          }
-        },
-        function (error) {
-          if (error) {
-            console.log(error)
-            res.status(500).send(error)
-          }
-        },
-      ),
-    client
-      .search({
-        index: elasticPersonIndex,
-        type: elasticSearchType,
-        pretty: true,
-        filter_path: 'hits.hits._source*',
-        q: `class:${key}`,
-        size: 10000,
-      })
-      .then(
-        function (body) {
-          if (
-            body &&
-            body.hits &&
-            body.hits.hits &&
-            body.hits.hits.length > 0
-          ) {
-            console.log('person search function called')
-            resultArray.push(...body.hits.hits)
-          }
-        },
-        function (error) {
-          if (error) {
-            console.log(error)
-            res.status(500).send(error)
-          }
-        },
-      ),
-    client
-      .search({
-        index: elasticClothingIndex,
-        type: elasticSearchType,
-        pretty: true,
-        filter_path: 'hits.hits._source*',
-        q: `class:${key}`,
-        size: 10000,
-      })
-      .then(
-        function (body) {
-          if (
-            body &&
-            body.hits &&
-            body.hits.hits &&
-            body.hits.hits.length > 0
-          ) {
-            console.log('clothing search function called')
-            resultArray.push(...body.hits.hits)
-          }
-        },
-        function (error) {
-          if (error) {
-            console.log(error)
-            res.status(500).send(error)
-          }
-        },
-      ),
-    client
-      .search({
-        index: elasticPpeIndex,
-        type: elasticSearchType,
-        pretty: true,
-        filter_path: 'hits.hits._source*',
-        q: `class:${key}`,
-        size: 10000,
-      })
-      .then(
-        function (body) {
-          if (
-            body &&
-            body.hits &&
-            body.hits.hits &&
-            body.hits.hits.length > 0
-          ) {
-            console.log('ppe search function called')
-            resultArray.push(...body.hits.hits)
-          }
-        },
-        function (error) {
-          if (error) {
-            console.log(error)
-            res.status(500).send(error)
-          }
-        },
-      ),
-    client
-      .search({
-        index: elasticDefectsIndex,
-        type: elasticSearchType,
-        pretty: true,
-        filter_path: 'hits.hits._source*',
-        q: `class:${key}`,
-        size: 10000,
-      })
-      .then(
-        function (body) {
-          if (
-            body &&
-            body.hits &&
-            body.hits.hits &&
-            body.hits.hits.length > 0
-          ) {
-            console.log('ppe search function called')
-            resultArray.push(...body.hits.hits)
-          }
-        },
-        function (error) {
-          if (error) {
-            console.log(error)
-            res.status(500).send(error)
-          }
-        },
-      ),
-  ])
-
-  const responseArray = []
-  let count = 0
-  if (resultArray.length > 0) {
-    for (const element of resultArray) {
-      const responseObj = {
-        image:
-          '/assets/shared-data/' +
-          element._source.image_path.split('/').splice(5, 5).join('/'),
-        width: element._source.cam_width,
-        height: element._source.cam_height,
-        checked: true,
-        resultObject: {
-          class: element._source.class,
-          boundingBox: {
-            left: element._source.x1,
-            top: element._source.y1,
-            width: element._source.x2 - element._source.x1,
-            height: element._source.y2 - element._source.y1,
-          },
-        },
-      }
-
-      // if any result data contains null NaN or undefined value, remove it
-      const checkingArray = []
-      Object.keys(responseObj).map(function (key) {
-        const value = responseObj[key]
-        if (
-          typeof value === 'undefined' ||
-          value === null ||
-          Object.is(value, NaN)
-        ) {
-          console.log('1st checking vehicle search >>>>>')
-          console.log(responseObj)
-          checkingArray.push(false)
-        }
-      })
-      if (responseObj.resultObject) {
-        Object.keys(responseObj.resultObject).map(function (key) {
-          const value = responseObj.resultObject[key]
-          if (
-            typeof value === 'undefined' ||
-            value === null ||
-            Object.is(value, NaN)
-          ) {
-            console.log('2nd checking vehicle search >>>>>')
-            console.log(responseObj.resultObject)
-            checkingArray.push(false)
-          }
-        })
-        if (responseObj.resultObject.boundingBox) {
-          Object.keys(responseObj.resultObject.boundingBox).map(function (key) {
-            const value = responseObj.resultObject.boundingBox[key]
-            if (
-              typeof value === 'undefined' ||
-              value === null ||
-              Object.is(value, NaN)
-            ) {
-              console.log('3rd checking vehicle search >>>>> ')
-              console.log(responseObj.resultObject.boundingBox)
-              checkingArray.push(false)
-            }
-          })
-        }
-      }
-
-      if (!checkingArray.includes(false)) {
-        responseArray.push(responseObj)
-      }
-    }
-  }
-  console.log(responseArray.length, '>>>>>>total result array length')
-  // preparing final response by concating result if duplicate image data found
-  const finalResponse = []
-  for (const resultData of responseArray) {
-    const resultObj = {
-      id: count,
-      image: resultData.image,
-      width: resultData.width,
-      height: resultData.height,
-      checked: resultData.checked,
-      results: {
-        Object: [],
-      },
-    }
-
-    for (let i = 0; i < finalResponse.length; i++) {
-      if (finalResponse[i].image === resultObj.image) {
-        resultObj['duplicate'] = true
-        finalResponse[i].results.Object.push(resultData.resultObject)
-      }
-    }
-
-    if (!resultObj.duplicate) {
-      resultObj.results.Object.push(resultData.resultObject)
-      finalResponse.push(resultObj)
-      ++count
-    }
-  }
-  console.log(finalResponse.length, '>>>>>>response array length')
-  res.status(200).send(finalResponse)
-})
-
-router.get('/analytics/elasticSearch-test/:key', async function (
-  req,
-  res,
-  next,
-) {
   const elasticSearchType = '_doc'
   const elasticIndexArray = [
     'vehicle_gsate',
@@ -623,115 +334,108 @@ router.get('/analytics/elasticSearch-test/:key', async function (
     })
     .then(
       function (data) {
+        const responseArray = []
+        let count = 0
         if (data && data.hits && data.hits.hits && data.hits.hits.length > 0) {
-          const responseArray = []
-          let count = 0
-          if (
-            data &&
-            data.hits &&
-            data.hits.hits &&
-            data.hits.hits.length > 0
-          ) {
-            for (const element of data.hits.hits) {
-              const responseObj = {
-                image:
-                  '/assets/shared-data/' +
-                  element._source.image_path.split('/').splice(5, 5).join('/'),
-                width: element._source.cam_width,
-                height: element._source.cam_height,
-                checked: true,
-                resultObject: {
-                  class: element._source.class,
-                  boundingBox: {
-                    left: element._source.x1,
-                    top: element._source.y1,
-                    width: element._source.x2 - element._source.x1,
-                    height: element._source.y2 - element._source.y1,
-                  },
+          for (const element of data.hits.hits) {
+            const responseObj = {
+              image:
+                '/assets/shared-data/' +
+                element._source.image_path.split('/').splice(5, 5).join('/'),
+              width: element._source.cam_width,
+              height: element._source.cam_height,
+              checked: true,
+              resultObject: {
+                class: element._source.class,
+                boundingBox: {
+                  left: element._source.x1,
+                  top: element._source.y1,
+                  width: element._source.x2 - element._source.x1,
+                  height: element._source.y2 - element._source.y1,
                 },
-              }
+              },
+            }
 
-              // if any result data contains null NaN or undefined value, remove it
-              const checkingArray = []
-              Object.keys(responseObj).map(function (key) {
-                const value = responseObj[key]
+            // if any result data contains null NaN or undefined value, remove it
+            const checkingArray = []
+            Object.keys(responseObj).map(function (key) {
+              const value = responseObj[key]
+              if (
+                typeof value === 'undefined' ||
+                value === null ||
+                Object.is(value, NaN)
+              ) {
+                console.log('1st checking vehicle search >>>>>')
+                console.log(responseObj)
+                checkingArray.push(false)
+              }
+            })
+            if (responseObj.resultObject) {
+              Object.keys(responseObj.resultObject).map(function (key) {
+                const value = responseObj.resultObject[key]
                 if (
                   typeof value === 'undefined' ||
                   value === null ||
                   Object.is(value, NaN)
                 ) {
-                  console.log('1st checking vehicle search >>>>>')
-                  console.log(responseObj)
+                  console.log('2nd checking vehicle search >>>>>')
+                  console.log(responseObj.resultObject)
                   checkingArray.push(false)
                 }
               })
-              if (responseObj.resultObject) {
-                Object.keys(responseObj.resultObject).map(function (key) {
-                  const value = responseObj.resultObject[key]
+              if (responseObj.resultObject.boundingBox) {
+                Object.keys(responseObj.resultObject.boundingBox).map(function (
+                  key,
+                ) {
+                  const value = responseObj.resultObject.boundingBox[key]
                   if (
                     typeof value === 'undefined' ||
                     value === null ||
                     Object.is(value, NaN)
                   ) {
-                    console.log('2nd checking vehicle search >>>>>')
-                    console.log(responseObj.resultObject)
+                    console.log('3rd checking vehicle search >>>>> ')
+                    console.log(responseObj.resultObject.boundingBox)
                     checkingArray.push(false)
                   }
                 })
-                if (responseObj.resultObject.boundingBox) {
-                  Object.keys(responseObj.resultObject.boundingBox).map(
-                    function (key) {
-                      const value = responseObj.resultObject.boundingBox[key]
-                      if (
-                        typeof value === 'undefined' ||
-                        value === null ||
-                        Object.is(value, NaN)
-                      ) {
-                        console.log('3rd checking vehicle search >>>>> ')
-                        console.log(responseObj.resultObject.boundingBox)
-                        checkingArray.push(false)
-                      }
-                    },
-                  )
-                }
               }
+            }
 
-              if (!checkingArray.includes(false)) {
-                responseArray.push(responseObj)
-              }
+            if (!checkingArray.includes(false)) {
+              responseArray.push(responseObj)
             }
           }
-          console.log(responseArray.length, '>>>>>>total result array length')
-          // preparing final response by concating result if duplicate image data found
-          const finalResponse = []
-          for (const resultData of responseArray) {
-            const resultObj = {
-              id: count,
-              image: resultData.image,
-              width: resultData.width,
-              height: resultData.height,
-              checked: resultData.checked,
-              results: {
-                Object: [],
-              },
-            }
-
-            for (let i = 0; i < finalResponse.length; i++) {
-              if (finalResponse[i].image === resultObj.image) {
-                resultObj['duplicate'] = true
-                finalResponse[i].results.Object.push(resultData.resultObject)
-              }
-            }
-
-            if (!resultObj.duplicate) {
-              resultObj.results.Object.push(resultData.resultObject)
-              finalResponse.push(resultObj)
-              ++count
-            }
-          }
-          console.log(finalResponse.length, '>>>>>>response array length')
-          res.status(200).send(finalResponse)
         }
+        console.log(responseArray.length, '>>>>>>total result array length')
+        // preparing final response by concating result if duplicate image data found
+        const finalResponse = []
+        for (const resultData of responseArray) {
+          const resultObj = {
+            id: count,
+            image: resultData.image,
+            width: resultData.width,
+            height: resultData.height,
+            checked: resultData.checked,
+            results: {
+              Object: [],
+            },
+          }
+
+          for (let i = 0; i < finalResponse.length; i++) {
+            if (finalResponse[i].image === resultObj.image) {
+              resultObj['duplicate'] = true
+              finalResponse[i].results.Object.push(resultData.resultObject)
+            }
+          }
+
+          if (!resultObj.duplicate) {
+            resultObj.results.Object.push(resultData.resultObject)
+            finalResponse.push(resultObj)
+            ++count
+          }
+        }
+        console.log(finalResponse.length, '>>>>>>response array length')
+        res.status(200).send(finalResponse)
       },
       function (error) {
         if (error) {
