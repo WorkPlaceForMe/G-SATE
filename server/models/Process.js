268d5182ffc8ee3a7a8f2600705ceafdaf8c7a87
Abhratanu Mukherjee <abhratanu@graymatics.com>
2021-09-30 21:19:06 +0530

    modification

--- server/models/Process.js
+++ server/models/Process.js
@@ -4,13 +4,14 @@ const moment = require('moment')
 var Process = {
   create: function (dataItems, callback) {
     return db.query(
-      'INSERT INTO vista_video_process (vista_operation_id, upload_id, subscriptions, timestamp) VALUES ?',
+      'INSERT INTO vista_video_process (vista_operation_id, upload_id, subscriptions, timestamp , created_at) VALUES ?',
       [
         dataItems.map((data) => [
           data.id,
           data.upload,
           data.subscriptions,
           moment(new Date(data.timestamp)).format('YYYY-MM-DD HH:mm:ss'),
+          moment().format('YYYY-MM-DD HH:mm:ss'),
         ]),
       ],
       callback,
