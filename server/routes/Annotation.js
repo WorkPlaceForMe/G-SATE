f452924ffc832fc7acc637a826e28bd8a4ba2a84
Abhratanu Mukherjee <abhratanu@graymatics.com>
2021-10-04 17:16:41 +0530

    es modified

--- server/routes/Annotation.js
+++ server/routes/Annotation.js
@@ -258,6 +258,142 @@ router.get('/vehicle/:key', function (req, res, next) {
   )
 })
 
+// router.get('/analytics/elasticSearch/:key', async function (req, res, next) {
+//   const elasticSearchType = '_doc'
+//   const elasticIndexArray = [
+//     'vehicle_gsate',
+//     'person_gsate',
+//     'clothing_gsate',
+//     'ppe_gsate',
+//     'defects_gsate',
+//   ]
+
+//   const { key } = req.params
+//   if (!key) return res.status(400).send('Search key is required')
+
+//   client
+//     .search({
+//       index: elasticIndexArray,
+//       type: elasticSearchType,
+//       pretty: true,
+//       filter_path: 'hits.hits._source*',
+//       q: `class:${key}`,
+//       size: 10000,
+//     })
+//     .then(
+//       function (data) {
+//         const responseArray = []
+//         let count = 0
+//         if (data && data.hits && data.hits.hits && data.hits.hits.length > 0) {
+//           for (const element of data.hits.hits) {
+//             const responseObj = {
+//               image:
+//                 '/assets/shared-data/' +
+//                 element._source.image_path.split('/').splice(5, 5).join('/'),
+//               width: element._source.cam_width,
+//               height: element._source.cam_height,
+//               checked: true,
+//               resultObject: {
+//                 class: element._source.class,
+//                 boundingBox: {
+//                   left: element._source.x1,
+//                   top: element._source.y1,
+//                   width: element._source.x2 - element._source.x1,
+//                   height: element._source.y2 - element._source.y1,
+//                 },
+//               },
+//             }
+
+//             // if any result data contains null NaN or undefined value, remove it
+//             const checkingArray = []
+//             Object.keys(responseObj).map(function (key) {
+//               const value = responseObj[key]
+//               if (
+//                 typeof value === 'undefined' ||
+//                 value === null ||
+//                 Object.is(value, NaN)
+//               ) {
+//                 console.log('1st checking elastic search >>>>>')
+//                 console.log(responseObj)
+//                 checkingArray.push(false)
+//               }
+//             })
+//             if (responseObj.resultObject) {
+//               Object.keys(responseObj.resultObject).map(function (key) {
+//                 const value = responseObj.resultObject[key]
+//                 if (
+//                   typeof value === 'undefined' ||
+//                   value === null ||
+//                   Object.is(value, NaN)
+//                 ) {
+//                   console.log('2nd checking elastic search >>>>>')
+//                   console.log(responseObj.resultObject)
+//                   checkingArray.push(false)
+//                 }
+//               })
+//               if (responseObj.resultObject.boundingBox) {
+//                 Object.keys(responseObj.resultObject.boundingBox).map(function (
+//                   key,
+//                 ) {
+//                   const value = responseObj.resultObject.boundingBox[key]
+//                   if (
+//                     typeof value === 'undefined' ||
+//                     value === null ||
+//                     Object.is(value, NaN)
+//                   ) {
+//                     console.log('3rd checking elastic search >>>>> ')
+//                     console.log(responseObj.resultObject.boundingBox)
+//                     checkingArray.push(false)
+//                   }
+//                 })
+//               }
+//             }
+
+//             if (!checkingArray.includes(false)) {
+//               responseArray.push(responseObj)
+//             }
+//           }
+//         }
+//         console.log(responseArray.length, '>>>>>>total result array length')
+//         // preparing final response by concating result if duplicate image data found
+//         const finalResponse = []
+//         for (const resultData of responseArray) {
+//           const resultObj = {
+//             id: count,
+//             image: resultData.image,
+//             width: resultData.width,
+//             height: resultData.height,
+//             checked: resultData.checked,
+//             results: {
+//               Object: [],
+//             },
+//           }
+
+//           for (let i = 0; i < finalResponse.length; i++) {
+//             if (finalResponse[i].image === resultObj.image) {
+//               resultObj['duplicate'] = true
+//               finalResponse[i].results.Object.push(resultData.resultObject)
+//             }
+//           }
+
+//           if (!resultObj.duplicate) {
+//             resultObj.results.Object.push(resultData.resultObject)
+//             finalResponse.push(resultObj)
+//             ++count
+//           }
+//         }
+//         console.log(finalResponse.length, '>>>>>>response array length')
+//         res.status(200).send(finalResponse)
+//       },
+//       function (error) {
+//         if (error) {
+//           console.log(error)
+//           res.status(500).send(error)
+//         }
+//       },
+//     )
+// })
+
 router.get('/analytics/elasticSearch/:key', async function (req, res, next) {
   const elasticSearchType = '_doc'
   const elasticIndexArray = [
@@ -267,6 +403,9 @@ router.get('/analytics/elasticSearch/:key', async function (req, res, next) {
     'ppe_gsate',
     'defects_gsate',
   ]
+
+  const vistaVideoProcessIndex = 'vista_video_process_gsate'
+  const vistaVideoProcessType = 'vista_video_process'
 
   const { key } = req.params
   if (!key) return res.status(400).send('Search key is required')
@@ -383,7 +522,37 @@ router.get('/analytics/elasticSearch/:key', async function (req, res, next) {
           }
         }
         console.log(finalResponse.length, '>>>>>>response array length')
-        res.status(200).send(finalResponse)
+        client
+          .search({
+            index: vistaVideoProcessIndex,
+            type: vistaVideoProcessType,
+            pretty: true,
+            filter_path: 'hits.hits._source*',
+            q: `class:${key}`,
+            size: 10000,
+          })
+          .then(
+            function (searchedData) {
+              const resoponse = {
+                analytics_processing: finalResponse,
+                vista_processing:
+                  searchedData &&
+                  searchedData.hits &&
+                  searchedData.hits.hits &&
+                  searchedData.hits.hits.length > 0
+                    ? searchedData.hits.hits
+                    : [],
+              }
+
+              res.status(200).send(resoponse)
+            },
+            function (errorResponse) {
+              if (errorResponse) {
+                console.log(errorResponse)
+                res.status(500).send(errorResponse)
+              }
+            },
+          )
       },
       function (error) {
         if (error) {
