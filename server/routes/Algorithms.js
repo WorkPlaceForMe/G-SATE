cc7fb8b527a397b23327609faf7b217c48023b0f
Abhratanu Mukherjee <abhratanu@graymatics.com>
2021-10-27 19:27:45 +0530

    test

--- server/routes/Algorithms.js
+++ server/routes/Algorithms.js
@@ -64,34 +64,11 @@ router.get('/custom', function (req, res, next) {
       res.json(err)
     } else {
       res.status(200).send(algos)
-      // Algorithm.create(
-      //   {
-      //     id: '38',
-      //     name: 'testing',
-      //     available: 1,
-      //   },
-      //   function (err, row) {
-      //     if (err) {
-      //       res.json(err)
-      //     } else {
-      //       Algorithm.createAlgorithmTable('testing', function (err, table) {
-      //         if (err) {
-      //           res.json(err)
-      //         } else {
-      //           res.status(200).send({
-      //             algorithmAdded: true,
-      //             message: 'Algorithm added',
-      //           })
-      //         }
-      //       })
-      //     }
-      //   },
-      // )
     }
   })
 })
 
-router.delete('/custom/:id', function (req, res, next) {
+router.delete('/customs/:id', function (req, res, next) {
   const customAlgoId = req.params.id
   Algorithm.getCustomAlgorithmById(customAlgoId, function (err, customAlgo) {
     if (err) {
@@ -146,6 +123,77 @@ router.delete('/custom/:id', function (req, res, next) {
             })
           }
         })
+      } else {
+        res.status(400).send({ message: 'Algorithm not found' })
+      }
+    }
+  })
+})
+
+router.delete('/custom/:id', function (req, res, next) {
+  const customAlgoId = req.params.id
+  Algorithm.getCustomAlgorithmById(customAlgoId, function (err, customAlgo) {
+    if (err) {
+      res.json(err)
+    } else {
+      if (customAlgo.length > 0) {
+        const dirName = `${process.env.modelsPath}${customAlgo[0].name}`
+        console.log(dirName, 'dirName')
+        if (fs.existsSync(dirName)) {
+          console.log('dir exists')
+          fs.rmdirSync(dirName, { recursive: true })
+          if (fs.existsSync(dirName)) {
+            console.log('dir not deleted')
+            return res
+              .status(400)
+              .send({ message: 'Not able to delete the algorithm' })
+          } else {
+            console.log('dir deleted')
+          }
+        }
+
+        // if (fs.existsSync(dirName)) {
+        //   console.log('dir not deleted')
+        //   return res
+        //     .status(400)
+        //     .send({ message: 'Not able to delete the algorithm' })
+        // } else {
+        //   console.log('dir deleted')
+        Algorithm.deleteAlgorithm(customAlgoId, function (err, algos) {
+          if (err) {
+            res.json(err)
+          } else {
+            console.log(`${customAlgo[0].name}_gsate`)
+            Algorithm.dropTable(`${customAlgo[0].name}_gsate`, function (
+              err,
+              success,
+            ) {
+              if (err) {
+                res.json(err)
+              } else {
+                Relations.deleteByAlgoId(customAlgoId, function (err, deleted) {
+                  if (err) {
+                    res.json(err)
+                  } else {
+                    Relations.deleteFromRelationR(customAlgoId, function (
+                      err,
+                      deleted,
+                    ) {
+                      if (err) {
+                        res.json(err)
+                      } else {
+                        res
+                          .status(200)
+                          .send({ message: 'Algorithm deleted successfully' })
+                      }
+                    })
+                  }
+                })
+              }
+            })
+          }
+        })
+        //   }
       } else {
         res.status(400).send({ message: 'Algorithm not found' })
       }
