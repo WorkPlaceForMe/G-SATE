746e47b617589a5127ded411a7317d658c4a9a5c
Abhratanu Mukherjee <abhratanu@graymatics.com>
2021-11-10 20:23:31 +0530

    ip changed

--- client/src/app/models/IpServer.ts
+++ client/src/app/models/IpServer.ts
@@ -5,4 +5,5 @@ export var tarinScriptIP: string = "13.76.172.134"; // Kartik's training Script
 //export var ip: string = "3.211.144.140"; // AWS
 // export var ip: string = "localhost";
 // ip of where the server is running 13.52.209.143
-export var trainStatusIp: string = "20.98.212.250";
+// export var trainStatusIp: string = "20.98.212.250";
+export var trainStatusIp: string = "20.119.220.118";
